f15f56ed85f69ed8d66953b5592b1499624bd0f1
KingFarGrace <55943642+KingFarGrace@users.noreply.github.com>
2021-06-06 16:06:43 +0800

    Delete KingFarType directory

--- KingFarType/src/com/KingFarGrace/main/Start.java
+++
@@ -1,9 +0,0 @@
-package com.KingFarGrace.main;
-
-import com.KingFarGrace.board.MainBoard;
-
-public class Start {
-	public static void main(String[] args) throws InterruptedException {
-		new MainBoard();	
-	}
-}
